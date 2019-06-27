848b4232dfe298b4c3296ed7f154cff111ce8243
Anton Strogonoff <anton@strogonoff.name>
2019-06-28 03:06:02 +0800

    feat(l10n): Implement {% trans_file %} tag

--- _plugins/localize.rb
+++ _plugins/localize.rb
@@ -46,6 +46,68 @@ module Jekyll
     end
 
   end
+
+  # Converts specified AsciiDoc file into HTML,
+  # using path <OB issue path>/<page.lang>-<filename>,
+  # falling back to <OB issue path>/<site.default_language>-<filename>,
+  # falling back to <OB issue path>/<filename>.
+  #
+  # Localization behavior resembles the one of the {% trans %} tag.
+  #
+  # Currently, only AsciiDoc files are supported.
+  #
+  # Example:
+  #
+  #   ```
+  #   ---
+  #   lang: en
+  #   file: iptn.adoc
+  #   ---
+  #
+  #   {% trans_file page.file %}
+  #   ```
+  class LocalizeProcessFile < Liquid::Tag
+
+    def initialize(tag_name, key, tokens)
+      super
+      @key = key.strip
+    end
+
+    def render(context)
+      filename = context[@key]
+      issue_path = File.join(
+        context['site']['source'],
+        context['site']['ob_root'],
+        context['page']['issue']['meta']['id'].to_s)
+
+      active_lang = context['page']['lang']
+      default_lang = context['site']['default_language']
+
+      fpath_candidates = [
+        File.join(issue_path, "#{active_lang}-#{filename}"),
+        File.join(issue_path, "#{default_lang}-#{filename}"),
+        File.join(issue_path, filename),
+      ]
+
+      result = nil
+
+      fpath_candidates.each do |fpath|
+        if File.file?(fpath)
+          contents = File.read(fpath)
+          result = Asciidoctor.convert contents, safe: :server
+          break
+        end
+      end
+
+      if result == nil
+        p "Couldn’t translate referenced file: #{filename}"
+      end
+
+      result
+    end
+
+  end
 end
 
 Liquid::Template.register_tag('trans', Jekyll::LocalizeKey)
+Liquid::Template.register_tag('trans_file', Jekyll::LocalizeProcessFile)
