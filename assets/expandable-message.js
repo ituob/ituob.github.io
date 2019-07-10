7fdceaac0e7c60ae57c46f975ec18173125163e4
Anton Strogonoff <anton@strogonoff.name>
2019-07-10 18:49:32 +0800

    feat: Message cards: Improve content display

--- assets/expandable-message.js
+++ assets/expandable-message.js
@@ -5,23 +5,30 @@
 
   var messageCards = document.querySelectorAll('article.message');
   for (let card of messageCards) {
-    if (card.querySelector('.included-contents')) {
+    var expandableContents = card.querySelector('.included-contents');
+    if (expandableContents) {
+      var header = Array.from(card.children).filter(e => e.matches('header'))[0];
+
+      if (header === undefined) { continue; }
+
       card.classList.add('with-js');
 
-      var chevron = document.createElement('i');
-      chevron.classList.add('fas', 'fa-chevron-circle-right');
-      chevron.addEventListener('click', function (evt) {
+      var title = header.querySelector('.title');
+      var label = title.querySelector('.label');
+
+      var toggle = document.createElement('div');
+      toggle.classList.add('toggle');
+      toggle.setAttribute('title', "Expand or collapse message contents");
+
+      var indicator = document.createElement('i');
+      indicator.classList.add('fas', 'fa-chevron-circle-down', 'indicator');
+      toggle.appendChild(indicator);
+
+      toggle.addEventListener('click', function (evt) {
         var expanded = card.classList.toggle('expanded');
-        if (expanded) {
-          evt.target.classList.remove('fa-chevron-circle-right');
-          evt.target.classList.add('fa-chevron-circle-down');
-        } else {
-          evt.target.classList.remove('fa-chevron-circle-down');
-          evt.target.classList.add('fa-chevron-circle-right');
-        }
       });
 
-      card.querySelector('header .title').prepend(chevron);
+      card.insertBefore(toggle, expandableContents);
     }
   }
 }());
