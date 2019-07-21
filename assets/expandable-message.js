f0efe5f6cceb81faa5fa7a0f43772b78ef1f8282
Anton Strogonoff <anton@strogonoff.name>
2019-07-21 22:56:58 +0900

    feat: Refactor expandable & apply to “about”

--- assets/expandable-message.js
+++ assets/expandable-message.js
@@ -2,35 +2,32 @@
 
   // Quick and dirty expandable message widget,
   // supported by FontAwesome and some CSS (see .with-js class)
+  window.initExpandable = function (card, expandableContents, iconNormal) {
+    card.classList.add('with-js');
+
+    var toggle = document.createElement('div');
+    toggle.classList.add('toggle');
+    toggle.setAttribute('title', "Expand or collapse message contents");
+
+    var indicator = document.createElement('i');
+    indicator.classList.add('fas', iconNormal, 'indicator');
+    toggle.appendChild(indicator);
+
+    toggle.addEventListener('click', function (evt) {
+      var expanded = card.classList.toggle('expanded');
+    });
+
+    card.insertBefore(toggle, expandableContents);
+
+    expandableContents.removeAttribute('style');
+  }
 
   var messageCards = document.querySelectorAll('article.message');
   for (let card of messageCards) {
     var expandableContents = card.querySelector('.included-contents');
     if (expandableContents) {
-      var header = Array.from(card.children).filter(e => e.matches('header'))[0];
-
-      if (header === undefined) { continue; }
-
-      card.classList.add('with-js');
-
-      var title = header.querySelector('.title');
-      var label = title.querySelector('.label');
-
-      var toggle = document.createElement('div');
-      toggle.classList.add('toggle');
-      toggle.setAttribute('title', "Expand or collapse message contents");
-
-      var indicator = document.createElement('i');
-      indicator.classList.add('fas', 'fa-chevron-circle-down', 'indicator');
-      toggle.appendChild(indicator);
-
-      toggle.addEventListener('click', function (evt) {
-        var expanded = card.classList.toggle('expanded');
-      });
-
-      card.insertBefore(toggle, expandableContents);
-
-      expandableContents.removeAttribute('style');
+      window.initExpandable(card, expandableContents, 'fa-chevron-circle-down');
     }
   }
+
 }());
