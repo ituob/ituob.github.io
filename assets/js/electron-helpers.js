973cccf663079500abd589def649d18f0af01ba7
Anton Strogonoff <anton@strogonoff.name>
2019-11-04 19:18:28 +0800

    refactor: electron-opener -> electron-helpers

--- assets/js/electron-helpers.js
+++ assets/js/electron-helpers.js
@@ -1,26 +1,37 @@
+// Detects Electron environment and returns the requested module, or nothing.
+
+(function () {
+  window.getElectronModule = function (moduleName) {
+    if (!window.require) {
+      return;
+    }
+
+    let mod;
+    try {
+      mod = require('electron')[moduleName];
+    } catch (e) {
+      return;
+    }
+
+    if (!mod) {
+      return;
+    }
+
+    return mod;
+  };
+}());
+
+
 // This module makes it so that if the page is loaded within Electron environment
 // (as part of built-in app help), then external links open in user’s regular web browser.
 //
-// It does so by detecting Electron environment (bailing if not detected),
-// and hijacking each external link’s click action,
+// Does so by hijacking each external link’s click action,
 // passing its href to `electron.shell.openExternal()` function
 // instead of navigating to the URL in the same window.
 
 (function () {
-  if (!window.require) {
-    return;
-  }
-
-  let shell;
-  try {
-    shell = require('electron').shell;
-  } catch (e) {
-    return;
-  }
-
-  if (!shell) {
-    return;
-  }
+  let shell = window.getElectronModule('shell');
+  if (!shell) { return; }
 
   Array.from(document.querySelectorAll('a')).forEach(a => {
     if (a.hostname.length > 0 && location.hostname !== a.hostname) {
@@ -34,3 +45,4 @@
     }
   });
 }());
+
